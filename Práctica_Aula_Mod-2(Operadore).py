b5da6d072c31cb46502d5098f902ee51f41fbd16
jpineda15 <juanfrancisco015@gmail.com>
2024-07-29 16:31:34 -0400

    update

---
+++ Práctica_Aula_Mod-2(Operadore).py
@@ -0,0 +1,27 @@
+# Práctica en el aula Mod-2
+# Programa que realiza las diferentes operaciones como (+,-,*,/)
+
+    # Declaramos la función operadores
+def operadores():
+        # Solicitamos la entrada de dos valores
+    num_1 = int(input("Ingrese el Primer Valor: "))
+    num_2 = int(input("Ingrese el Segundo Valor: "))
+        # Realizamos las operaciones matemáticas correspondientes
+    resul1 = num_1 + num_2
+    resul2 = num_1 - num_2
+    resul3 = num_1 * num_2
+    resul4 = num_1 / num_2
+    resul5 = num_1 // num_2
+    resul6 = num_1 % num_2
+    resul7 = num_1 ** num_2
+        # Imprimimos los resultados de las distintas operaciones
+    print(f"La Suma de {num_1} + {num_2} = {resul1}")
+    print(f"La resta de {num_1} - {num_2} + {resul2}")
+    print(f"La multiplicacion de {num_1} * {num_2} = {resul3}")
+    print(f"La División de {num_1} / {num_2} = {resul4}")
+    print(f"División Entera de {num_1} // {num_2} = {resul5}")
+    print(f"El Módulo (resto de la división) de  {num_1} % {num_2} = {resul6}")
+    print(f"La Exponenciación de {num_1} ** {num_2} = {resul7}")
+
+# Llamamos la función operadores
+operadores()
