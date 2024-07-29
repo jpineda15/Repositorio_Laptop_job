4350ddb450a07c9e44715ca707b703da801eef19
jpineda15 <juanfrancisco015@gmail.com>
2024-07-29 19:13:28 -0400

    update

--- Práctica_Aula_Mod_2.py
+++ Práctica_Aula_Mod_2.py
@@ -5,5 +5,5 @@
 def cien_year(): # Creamos la Funcion cien_year
     año = int(input("Ingrese el año de Nacimiento: ")) # Solicitamos al Usuario que ingrese el Año de Nacimiento
     cien_años = año + 100 # Calculamos el año en que el Usuario cumplirá 100 año
-    print("Usted Cumplirá 100 años en el año", cien_años) # Imprimimos el resultado
+    print(f"Usted Cumplirá 100 años en el año {cien_años}") # Imprimimos el resultado
 cien_year() # Llamamos la funcion que contiene el bloque de codigo
