21e530fad440df79621a6b47e601f2535f3527a6
jpineda15 <juanfrancisco015@gmail.com>
2024-08-01 21:14:05 -0400

    update

---
+++ Generadores.py
@@ -0,0 +1,45 @@
+'''
+Venta de Alcohol:
+    Pregunta al usuario su edad.
+    Utiliza un operador logico para dertrminar si el usuario tiene la edad suficiente para comprar.
+    alcohol en funcion de la legilacion local (por Ejemplo, 18 años). 
+    Muestra un mensaje indicando si el susario puede comprar alcohol o no.
+'''
+'''
+Analisis del proyocto.
+    1.  Motrar el nomre del programa:
+        "Locor Stop El Bebedor"
+    2.  Dejar una linea en blanco
+        dejamos una linea en blanco para mejor visualizacion
+    3.  Solicitar la edad del Usuario
+        "Digite su edad"
+    4.  Validar la edad:
+        convertir la edad ingresad en numero entero
+        si la edad es 18 años o mas
+            Motrar en pantalla "Eres Mayor de edad"
+        si la edad es manor  18 años o más:
+            Mostrar en la pantalla el mensaje "No tiene la edad suficiente."
+    5.  Agradecer al usuario
+        Mostrar en la pantalla el mensaje "Gracias por preferirnos"
+    
+
+# Solucion 
+print("Licor Stop EL Bebedor") 
+print()
+edad = int(input("Digiste su edad: "))
+if edad >= 18:
+    print("Eres Mayor de edad.")
+else:
+    print("No tiene la edad susficiente.")
+print("Gracia por preferirnos")
+'''
+
+'''
+Aprobacion de Credito:
+
+pregunta al usuario si tiene un trabajo estable (si/no).
+prguenta al usuario si tiene un buen historial crediticio (si/no).
+utiliza un operador logico para determinar si el usuario es elegible para obtener un credito
+Muestra un mensaje indicando si elusuario es elegible o no. 
+
+''''
