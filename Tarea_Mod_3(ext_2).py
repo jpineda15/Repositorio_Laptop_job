439574e21646fd3b329b93b7dc70aaf07ad86637
jpineda15 <juanfrancisco015@gmail.com>
2024-08-02 16:50:35 -0400

    update

--- Tarea_Mod_3(ext_2).py
+++ Tarea_Mod_3(ext_2).py
@@ -15,7 +15,7 @@ print(f"Los Colores quinto, sexto y séptimos: {colorlist}.")
 
 
 colorlist = ['negro', 'azul', 'marrón', 'gris', 'verde', 'naranja', 'rosa', 'púrpura']
-colorlist.remove("marrón")
+colorlist.remove("marrón") 
 print(f"Eliminamos el Color 'marrón': {colorlist}.")
 
 colorlist.pop(3)
@@ -25,19 +25,29 @@ print(f"Remueva el tercer elemento especificando el índice: {colorlist}.")
 print(f"Imprima el número de elementos de la lista: {len(colorlist)}")
 
 
-'''
+
+
+
 def colores():
+    colorList = [] 
+    colorList = ['negro', 'azul', 'marrón', 'gris', 'verde', 'naranja', 'rosa', 'púrpura']
+    colorList.append("Magenta")
+    print(colorList)
     
+    new_color = ["Blanco", "Amarillo"] #-->nueva lista con valores a reemplazar. 
+    indeX = colorList.index('marrón') #--> Almacenamos el Indice del valor a reemplazar. 
+    colorList = colorList[:indeX] + new_color + colorList[indeX + 1:]
+    print(f"Lista Completa: {colorList}")
     
-    # Usamos el Método append() para agregar el color Magenta a la colorList. 
-    
+    colorlist.remove("marrón") # --> El color marrón fue removido, estaremos colocando el que lo sustituyo
+    #print(f"Eliminamos el Color 'marrón': {colorlist}.")
     #listColor = " ," .join(colorList)
-    print(f"Los Colores de Mi Lista: {listColor}.")
-    cololist = colorList[5:8]
+    #print(f"Los Colores de Mi Lista: {listColor}.")
+    #cololist = colorList[5:8]
     #colorlist = ", " .join(cololist) # Unir los elementos de colorList en una sola cadena, separados por comas y espacios.
     
-    colorList = list(colorlist)
+    #colorList = list(colorlist)
     #colorList.remove("azul") # Eliminamos el segundo elemento de nuestra lista. 
     
 colores()
-'''
+
