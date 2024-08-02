7444a31a4c4cbf824a89e0d26f4fafc6fd486df0
jpineda15 <juanfrancisco015@gmail.com>
2024-08-02 16:01:32 -0400

    update

--- Tarea_Mod_3(ext_2).py
+++ Tarea_Mod_3(ext_2).py
@@ -7,10 +7,10 @@ def colores():
     new_color = ["Blanco", "Amarillo"] #-->nueva lista con valores a reemplazar. 
     indeX = colorList.index('marrón') #--> Almacenamos el Indice del valor a reemplazar. 
     colorList = colorList[:indeX] + new_color + colorList[indeX + 1:]
-    listColor = " ," .join(colorList)
+    #listColor = " ," .join(colorList)
     print(f"Los Colores de Mi Lista: {listColor}.")
     cololist = colorList[5:8]
-    colorlist = ", " .join(cololist) # Unir los elementos de colorList en una sola cadena, separados por comas y espacios.
+    #colorlist = ", " .join(cololist) # Unir los elementos de colorList en una sola cadena, separados por comas y espacios.
     print(f"Los Colores quinto, sexto y séptimos: {colorlist}.")
     colorList = list(colorlist)
     #colorList.remove("azul") # Eliminamos el segundo elemento de nuestra lista. 
