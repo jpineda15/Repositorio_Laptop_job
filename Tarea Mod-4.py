1ddd8a5a0a287f0a1349596dd937d005735b7488
jpineda15 <juanfrancisco015@gmail.com>
2024-08-09 14:57:53 -0400

    update

--- Tarea Mod-4.py
+++ Tarea Mod-4.py
@@ -1,4 +1,7 @@
 """
+Crea una lista de frutas que contenga 10 elementos y por cada fruta imprime 
+su nombre, pero también cuenta la cantidad de caracteres que tiene dicha fruta, 
+utiliza un ciclo for y un ciclo while:
 """
 
 def frutListW(): # Utilizamos el bucle while
@@ -10,9 +13,11 @@ def frutListW(): # Utilizamos el bucle while
             'Carambola', 'Limón']
     i = 0
     while i < len(frutas):
-        print(f"En nuestra lista tenemos: '{frutas[i]}' y tiene '{len(frutas[i])}' Caracteres.")
+        print(f"En nuestra lista tenemos: '{frutas[i]}' tiene '{len(frutas[i])}' Caracteres.")
         i += 1
 frutListW()
+
+print("###################################################")
 
 def frutListF(): # Utilizamos el bucle for
     frutas = [
@@ -22,5 +27,5 @@ def frutListF(): # Utilizamos el bucle for
             'Sandia', 'Papaya',
             'Carambola', 'Limón']
     for a in frutas:
-        print(f"En nuestra lista tenemos: '{a}' y tiene '{len(a)}' Caracteres.")
+        print(f"En nuestra lista tenemos: '{a}' tiene '{len(a)}' Caracteres.")
 frutListF()
