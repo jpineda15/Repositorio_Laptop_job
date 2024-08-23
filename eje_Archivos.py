2545a1be41660558de9c3642fa947349d8c86551
jpineda15 <juanfrancisco015@gmail.com>
2024-08-22 20:44:24 -0400

    update

--- eje_Archivos.py
+++ eje_Archivos.py
@@ -1,13 +1,15 @@
 '''
 1 readlines() --> nos permite imprimir la primera Linea.
 
-r --> read(lectura)
+r --> read(lectura) --> Leer todo el contenido de archivo
+
 t --> text-mode
 w --> write(escritura)
 a --> append(Agregar contenido)
 x --> create 
 b --> bytes - para archivos como fotos
 encoding = 'utf-8' --> esta parte es optional(codificación de caracteres)
+write --> escribir en un archivo. 
 '''
 """
 File = open('C:/Users/jpineda/Desktop/PythonGit/fichero.txt', 'r', encoding = 'utf-8')
@@ -22,8 +24,8 @@ File.close() #Cerramos el documento
 """
 # with nos permite cerrar el archivo automáticamente termine de usarlo.
 with open('C:/Users/jpineda/Desktop/PythonGit/data.txt', 'r') as archivo:
-    oneLine = archivo.readline()
-    linea = archivo.readlines()
+    oneLine = archivo.readline() # --> Leer un línea del archivo
+    linea = archivo.readlines() # --> Leer 
     print(linea)
     print("Nos muestra una Linea del archivo: ", oneLine)
 
@@ -35,7 +37,7 @@ for i in linea:
 with open('C:/Users/jpineda/Desktop/PythonGit/data.txt', 'r') as archivo:
     print(archivo.read().split('\n')) # read(r) leer | split('\n') eliminamos el salto de linea del \n
 
-with open('./data.txt', 'r') as archivo:
-    print(archivo.read().split('\n')) 
+'''with open('./data.txt', 'r') as archivo:
+    print(archivo.read().split('\n')) '''
 
 #fichero = open('./datas.txt', 'rt', encoding='utf-8')
